41f5e5be563bca21000c68b8fb2b19e704ffc6e9
Anand Dikule <ananddikule@gmail.com>
2024-05-05 23:00:29 +0530

    added four more test cases for different steps

--- src/test/java/com/example/chandrayan_3_spacecarft_control/ChandrayanSpaceCraftControlTest.java
+++ src/test/java/com/example/chandrayan_3_spacecarft_control/ChandrayanSpaceCraftControlTest.java
@@ -1,26 +1,74 @@
 package com.example.chandrayan_3_spacecarft_control;
 
-import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.FixMethodOrder;
 import org.junit.Test;
+import org.junit.runners.MethodSorters;
 
 import static org.junit.Assert.*;
 
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
 public class ChandrayanSpaceCraftControlTest {
 
-    private ChandrayanSpaceCraftControl spaceCraft;
+    private static ChandrayanSpaceCraftControl spaceCraft;
 
-    @Before
-    public void setUp() {
+    @BeforeClass
+    public static void setUp() {
         spaceCraft = new ChandrayanSpaceCraftControl(0, 0, 0, Direction.N);
     }
 
     @Test
-    public void testSequenceOfCommands() {
-        spaceCraft.sendCommands(new String[]{"f", "r", "u", "b", "l"});
+    public void test1MoveForeword() {
+        spaceCraft.sendCommands(new String[]{"f"});
+        assertEquals(0, spaceCraft.getX());
+        assertEquals(1, spaceCraft.getY());
+        assertEquals(0, spaceCraft.getZ());
+        assertEquals(Direction.N, spaceCraft.getDirection());
+    }
+
+    @Test
+    public void test2TurnRight() {
+        spaceCraft.sendCommands(new String[]{"r"});
+        assertEquals(0, spaceCraft.getX());
+        assertEquals(1, spaceCraft.getY());
+        assertEquals(0, spaceCraft.getZ());
+        assertEquals(Direction.E, spaceCraft.getDirection());
+    }
+
+    @Test
+    public void test3MoveUp() {
+        spaceCraft.sendCommands(new String[]{"u"});
+        assertEquals(0, spaceCraft.getX());
+        assertEquals(1, spaceCraft.getY());
+        assertEquals(0, spaceCraft.getZ());
+        assertEquals(Direction.Up, spaceCraft.getDirection());
+    }
+
+    @Test
+    public void test4MoveBack() {
+        spaceCraft.sendCommands(new String[]{"b"});
+        assertEquals(0, spaceCraft.getX());
+        assertEquals(1, spaceCraft.getY());
+        assertEquals(-1, spaceCraft.getZ());
+        assertEquals(Direction.Up, spaceCraft.getDirection());
+    }
+
+    @Test
+    public void test5TurnLeft() {
+        spaceCraft.sendCommands(new String[]{"l"});
         assertEquals(0, spaceCraft.getX());
         assertEquals(1, spaceCraft.getY());
         assertEquals(-1, spaceCraft.getZ());
         assertEquals(Direction.N, spaceCraft.getDirection());
     }
 
+    @Test
+    public void test6ResetAndTestWithArray(){
+        spaceCraft = new ChandrayanSpaceCraftControl(0, 0, 0, Direction.N);
+        spaceCraft.sendCommands(new String[]{"f", "r", "u", "b", "l"});
+        assertEquals(0, spaceCraft.getX());
+        assertEquals(1, spaceCraft.getY());
+        assertEquals(-1, spaceCraft.getZ());
+        assertEquals(Direction.N, spaceCraft.getDirection());
+    }
 }
